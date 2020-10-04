70206a69941810968f721aa3e0130acf64027f41
Eric Allen <aperic234@gmail.com>
2020-10-03 19:59:26 -0600

    fixed?

--- web/assignments/php/component.php
+++ web/assignments/php/component.php
@@ -1,13 +1,13 @@
 <?php 
 function component(){
-  $element="
-  <div class='item sale1'>
-    <img src='images/turtle.jpg'>
+  $element='
+  <div class="item sale1">
+    <img src="images/turtle.jpg">
     <ul>
-      <li class='name'>turtle</li>
-      <li class='price'>500</li>
+      <li class="name">turtle</li>
+      <li class="price">500</li>
     </ul>
   </div>
-  ";
+  ';
   echo $element;
 }
