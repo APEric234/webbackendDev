5130b035beb479471803f4a490e3f7420631af79
Eric Allen <aperic234@gmail.com>
2020-10-03 19:43:48 -0600

    now has some fun php

---
+++ web/assignments/php/component.php
@@ -0,0 +1,12 @@
+<?php 
+function component(){
+  $element="
+  <div class='item sale1'>
+    <img src='images/turtle.jpg'>
+    <ul>
+      <li class='name'>turtle</li>
+      <li class='price'>500</li>
+    </ul>
+  </div>
+  ";
+}
---
+++ web/assignments/week3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+  <head>
+    <link rel="stylesheet" href="week3.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  </head>
+  <title>
+    Shopping cart simple
+  </title>
+  <div class="items">
+  <?php 
+  component();
+  component();
+  component();
+  component();
+  component();
+  ?>
+  
+</div>
+</body>
+</html>
