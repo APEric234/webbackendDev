e8b79754319acc37440a63efa8c8411d9bdda62e
Eric Allen <aperic234@gmail.com>
2020-10-03 20:06:44 -0600

    quick change

--- web/index.php
+++ web/index.php
@@ -1,29 +1,5 @@
-<!DOCTYPE html>
+<?php
 
-<html>
-  <head> 
-      <link rel="stylesheet" href="homepagestly.css">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-      <script src="homePage.JS"></script>
-    </head>
-  <title>
-      Eric Allen Home Page
+  phpinfo();
 
-  </title>
-  <body>
-    
-  <ul>
-    <li>
-      <a href="allwork.html">Assignments</a>
-    </li>
-  </ul>
-  <img src="images/turtle.jpg"> This ISN'T ME </img>
-  <div class="btn-group container-lg btn-group-lg justify-content-center align-items-center" role="group" >
-    <input type=text class="diceIn">How many sides does the best dice have? </input>
-    <button class='btn-secondary dice ' >Click to test your answer</button>
-      </div>
-      <div><p>Short intro about me, am a long time board/tabletop game enthusiast and true enthusiast can answer the above question :) I am studying to improve my career prospects as a developer</p></div>
-      <?php echo date("D M d, Y G:i a");?>
-    </body>
- </html>
+?>
