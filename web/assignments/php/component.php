21da3117fa744bf9da6e1281e9ccc4ac31d05fc4
Eric Allen <aperic234@gmail.com>
2020-10-03 20:15:19 -0600

    fixed

--- web/assignments/php/component.php
+++ web/assignments/php/component.php
@@ -4,8 +4,8 @@ function component($name,$price){
   <div class="item sale1">
     <img src="images/turtle.jpg">
     <ul>
-      <li class="name">'+ $name +'</li>
-      <li class="price">'+$price+'</li>
+      <li class="name">$name </li>
+      <li class="price">$price</li>
     </ul>
   </div>
   ';
