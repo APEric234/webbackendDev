4ef0d92906e2778731712f80b48e6a192fca09a0
Eric Allen <aperic234@gmail.com>
2020-10-17 19:12:25 -0600

    please just work

--- web/game_searched.php
+++ web/game_searched.php
@@ -1,44 +1,63 @@
 <!DOCTYPE html>
 
 <html>
-
+  
 <?php
 require_once('db.php')
 ?>
+  <head> 
+      <link rel="stylesheet" href="homepagestly.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="homePage.JS"></script>
+    </head>
+  <title>
+      Games Forum
 
-<head>
-  <link rel="stylesheet" href="homepagestly.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="homePage.JS"></script>
-</head>
-<title>
-  Games Forum
-
-</title>
-<h1>Welcome to Games For US!</h1>
-<P>Here we hope to fulfill all your gaming needs!</P>
-<p> Your search returned the following games</P>
-<ul>
-
-</ul>
-<P>feel free to browse our other amazing games</P>
-<ul>
+  </title>
+  <h1>Welcome to Games For US!</h1>
+  <P>Here we hope to fulfill all your gaming needs!</P>
+  <p> Your search returned the following games</P>
+  <ul>
   <?php
 $db = get_db();
 
 try{
+  $game = $_POST["game"];
+  echo($game);
 
+  $query = "select * from games where name_game = '"+ $game+ "';" ;
+  
+  $count = 0;
+  echo($query);
+  $stmnt = $db->query($query);
+  $stmnt -> execute();
+  $games = $stmnt->fetch();
+  echo($games);
+  echo("<li class='game'>The game <b>"+$games['game_name']+"</b> It has the genre of"+ $games['genre'] +" and is in stock</li></br></br>");
+  $count = $count+1;
+  }
 
+}catch(Exception $ex){
+  echo($ex);
+}
+?>
+</ul>
+<P>feel free to browse our other amazing games</P>
+<ul>
+<?php
+$db = get_db();
+
+try{
+ 
   $query = "select * from games;";
   $count = 0;
   echo($query);
   $stmnt = $db->query($query);
   $stmnt -> execute();
-  $games = $stmnt->fetchAll();
+  $games = $stmnt->fetch();
   echo($games);
   foreach ($games as $gamey){
-    echo("got here");
     echo("<li class='game'>The game is <b>"+$gamey['game_name']+"</b> It has the genre of"+ $gamey['genre'] +" and is in stock</li></br></br>");
     $count = $count+1;
     }
@@ -50,7 +69,6 @@ try{
 ?>
 </ul>
 
-</body>
-<footer><?php echo date("D M d, Y G:i a");?></footer>
-
-</html>
+    </body>
+    <footer><?php echo date("D M d, Y G:i a");?></footer>
+ </html>
