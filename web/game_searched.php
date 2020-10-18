bece76bbb8b8e5a482da674cfcbdf0e20ac8076b
Eric Allen <aperic234@gmail.com>
2020-10-17 19:23:26 -0600

    maybe this one?

--- web/game_searched.php
+++ web/game_searched.php
@@ -28,13 +28,9 @@ try{
   $game = $_POST["game"];
   echo($game);
 
-  $query = "select * from games where name_game = '"+ $game+ "';" ;
-  
-  $count = 0;
-  echo($query);
-  echo("  ");
-  $stmnt = $db->query($query);
-  $stmnt -> execute();
+  #$query = "select * from games where name_game = '"+ $game+ "';" ;
+  $stmt = $db->prepare('SELECT * FROM games WHERE name_game = ?');
+  $stmnt -> execute([$game]);
   $games = $stmnt->fetch();
   if($games){
   echo("<li class='game'>The game <b>"+$games['game_name']+"</b> It has the genre of"+ $games['genre'] +" and is in stock</li></br></br>");
