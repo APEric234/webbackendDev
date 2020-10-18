f0beb3366fece42f6e2ca51bc6401f03c969ffbf
Eric Allen <aperic234@gmail.com>
2020-10-17 18:59:43 -0600

    beautify

--- web/game_searched.php
+++ web/game_searched.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
 
 <html>
-  
+
 <?php
 require_once('db.php')
 ?>
-  <head> 
-      <link rel="stylesheet" href="homepagestly.css">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-      <script src="homePage.JS"></script>
-    </head>
-  <title>
-      Games Forum
 
-  </title>
-  <h1>Welcome to Games For US!</h1>
-  <P>Here we hope to fulfill all your gaming needs!</P>
-  <p> Your search returned the following games</P>
-  <ul>
+<head>
+  <link rel="stylesheet" href="homepagestly.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="homePage.JS"></script>
+</head>
+<title>
+  Games Forum
+
+</title>
+<h1>Welcome to Games For US!</h1>
+<P>Here we hope to fulfill all your gaming needs!</P>
+<p> Your search returned the following games</P>
+<ul>
   <?php
 $db = get_db();
 
@@ -44,7 +45,8 @@ try{
 ?>
 </ul>
 <P>feel free to browse our other amazing games</P>
-<?php
+<ul>
+  <?php
 $db = get_db();
 
 try{
@@ -68,8 +70,9 @@ try{
   echo($ex);
 }
 ?>
+</ul>
 
+</body>
+<footer><?php echo date("D M d, Y G:i a");?></footer>
 
-    </body>
-    <footer><?php echo date("D M d, Y G:i a");?></footer>
- </html>
+</html>
