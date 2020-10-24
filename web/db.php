df4a038bf6d4291cd211a536dbe2a3798eefffb4
Eric Allen <aperic234@gmail.com>
2020-10-24 12:24:16 -0600

    more debugging

--- web/db.php
+++ web/db.php
@@ -10,6 +10,7 @@ $dbPort = $dbOpts["port"];
 $dbUser = $dbOpts["user"];
 $dbPassword = $dbOpts["pass"];
 $dbName = ltrim($dbOpts["path"],'/');
+print "$dbHost,$dbPort,$dbName";
 $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }catch(PDOException $ex){
