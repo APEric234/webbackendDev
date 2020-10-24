91223e2321cdb646ed510b5453d9d884c5203882
Eric Allen <aperic234@gmail.com>
2020-10-24 11:34:07 -0600

    fixed css mistake

--- web/games.php
+++ web/games.php
@@ -3,7 +3,7 @@
 <html>
 
 <head>
-  <link rel="stylesheet" href="homepagestly.css">
+  <link rel="stylesheet" href="game_searched.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="homePage.JS"></script>
