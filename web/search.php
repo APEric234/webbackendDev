a9ee44343e60d412fccf1057c1d6899288d4f708
Eric Allen <aperic234@gmail.com>
2020-10-24 10:41:21 -0600

    compartamentalized script for game search

---
+++ web/search.php
@@ -0,0 +1,23 @@
+<?php 
+require_once('db.php');
+$db = get_db();
+
+try{
+  $game = $_POST["game"];
+  echo($game);
+
+  #$query = "select * from games where name_game = '"+ $game+ "';" ;
+  $stmt = $db->prepare('SELECT * FROM games WHERE name_game = ?');
+  $stmnt -> execute([$game]);
+  $games = $stmnt->fetch();
+  if($games){
+  echo("<li class='game'>The game <b>"+$games['game_name']+"</b> It has the genre of"+ $games['genre'] +" and is in stock</li></br></br>");
+  $count = $count+1;
+}
+  
+
+}catch(Exception $ex){
+  echo($ex);
+}
+?>
+ 
