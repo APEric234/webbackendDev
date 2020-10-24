f4a8b0c88ba6ca7757f86aee4ed708caa424d11b
Eric Allen <aperic234@gmail.com>
2020-10-24 14:52:27 -0600

    fixed query execution

--- web/search.php
+++ web/search.php
@@ -4,16 +4,19 @@ $db = get_db();
 
 try{
   $game = $_POST["game"];
-  echo($game);
   $sql = 'SELECT * from games;';
   $result =$db->query($sql);
   $result -> execute();
   $result = $result ->fetch(PDO::FETCH_ASSOC);
-  print_r($result);
+
   if($result->num_rows>=1){
   foreach ($result as $row) {
-      print "got here";
-      print $row["name_game"];
+    if ($row["name_game"] == $game){
+      echo "<li class=game> We have a copy of <b>" ,$row["name_game"],"</b> we have it categorized as a ",
+      $row["Type_game"]," type game of the ",$row["genre"]," genre </li>";
+
+  
+    }
   }
 }
 
