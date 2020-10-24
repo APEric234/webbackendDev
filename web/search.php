9eaea29e828658bdb0f530b7054b1a618d0d089d
Eric Allen <aperic234@gmail.com>
2020-10-24 11:55:05 -0600

    changed to check it not in the query string

--- web/search.php
+++ web/search.php
@@ -6,18 +6,20 @@ try{
   $game = $_POST["game"];
   echo($game);
 
-  $query_string = "select * from games where name_game = '"+ $game+ "';" ;
+  $query_string = "select * from games;" ;
 
   
   foreach ($db->query($query_string) as $row) {
-  echo("<li class='game'>The game <b>"+$row['game_name']+"</b> It has the genre of"+ $row['genre'] +" and is in stock</li></br></br>");
+    if($row['game_name'] == $game){
+      echo("<li class='game'>The game <b>"+$row['game_name']+"</b> It has the genre of"+ $row['genre'] +" and is in stock</li></br></br>");
+    }
 
 }
   
 
 }catch(Exception $ex){
   echo("there was an error");
-  #echo($ex);
+  echo($ex);
 }
 ?>
  
