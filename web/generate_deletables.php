563fcff7cd95320795e20a7adc645b931da373d3
Eric Allen <aperic234@gmail.com>
2020-10-24 17:44:47 -0600

    form

--- web/generate_deletables.php
+++ web/generate_deletables.php
@@ -10,8 +10,10 @@ try{
   $result = $result ->fetchAll(PDO::FETCH_ASSOC);
 
   foreach ($result as $row) {
-      echo "<form submit='delete.php' method='post'><input id='gameId' name='gameId' type='hidden' value=", $row["game_id"], ">";
-      echo "<input type='submit' value='Delete", $row["name_game"],"'>";
+      echo "<form submit='delete.php' method='post'>";
+      echo "<input id='gameId' name='gameId' type='hidden' value=", $row["game_id"], ">";
+      echo "<input type='submit' value='Delete ", $row["name_game"],"'>";
+      echo "</form>";
   }
 
 
