fde350aef878f72cb9455b27d547fe2ae53feb02
Eric Allen <aperic234@gmail.com>
2020-10-24 12:13:50 -0600

    testing

--- web/search.php
+++ web/search.php
@@ -5,18 +5,12 @@ $db = get_db();
 try{
   $game = $_POST["game"];
   echo($game);
-
-  $query_string = "select * from games;" ;
-  echo($query_string);
-
-  
-  foreach ($db->query($query_string) as $row) {
-    echo $row['game_name']; 
-    if($row['game_name'] == $game){
-      echo "<li class='game'>The game <b>",$row['game_name'],"</b> It has the genre of", $row['genre'] ," and is in stock</li></br></br>";
-    }
-
-}
+  $sql = 'SELECT * from games;';
+  foreach ($conn->query($sql) as $row) {
+      print $row['game_name'] . "\t";
+      print $row['genre'] . "\t";
+      print $row['type_game'] . "\n";
+  }
   
 
 }catch(Exception $ex){
