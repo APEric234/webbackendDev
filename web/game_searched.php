c6364411b6e845a96b7c0260d3feed94389d0a77
Eric Allen <aperic234@gmail.com>
2020-10-24 11:18:28 -0600

    does this work?

--- web/game_searched.php
+++ web/game_searched.php
@@ -7,6 +7,7 @@
 
 
 <head>
+  <?php require_once('search.php'); ?>
   <link rel="stylesheet" href="homepagestly.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -21,19 +22,19 @@
 
   <h1>Welcome to Games For US!</h1>
   <div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="genre.php">Genres</a>
-  <a href="wholecatalog.php">Contact</a>
-  <a href="about.html">About</a>
-</div>
+    <a class="active" href="#home">Home</a>
+    <a href="genre.php">Genres</a>
+    <a href="wholecatalog.php">Contact</a>
+    <a href="about.html">About</a>
+  </div>
   <P>Here we hope to fulfill all your gaming needs!</P>
-  <p> Your search returned the following games 
+  <p> Your search returned the following games
     <ul class="search_results">
-  <?php require_once('search.php'); ?>
+
 
     </ul>
-</P>
-  
+  </P>
+
 
 </body>
 <footer><?php echo date("D M d, Y G:i a");?></footer>
