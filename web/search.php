31ae2e9f5f8f6550f33f7a2fca9be373a55ee56b
Eric Allen <aperic234@gmail.com>
2020-10-24 11:52:41 -0600

    am i doing it right now?

--- web/search.php
+++ web/search.php
@@ -6,12 +6,11 @@ try{
   $game = $_POST["game"];
   echo($game);
 
-  #$query = "select * from games where name_game = '"+ $game+ "';" ;
-  $stmt = $db->prepare("SELECT * FROM games WHERE name_game = '?';");
-  $stmnt -> execute([$game]);
-  $games = $stmnt->fetch();
-  if($games){
-  echo("<li class='game'>The game <b>"+$games['game_name']+"</b> It has the genre of"+ $games['genre'] +" and is in stock</li></br></br>");
+  $query_string = "select * from games where name_game = '"+ $game+ "';" ;
+
+  $games = $db->query($y);
+  foreach ($db->query($query_string) as $row) {
+  echo("<li class='game'>The game <b>"+$row['game_name']+"</b> It has the genre of"+ $row['genre'] +" and is in stock</li></br></br>");
 
 }
   
