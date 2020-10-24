0d1060ae55a9fa81d54f99c37ae8761ab0cf377f
Eric Allen <aperic234@gmail.com>
2020-10-24 11:53:00 -0600

    removed bad line

--- web/search.php
+++ web/search.php
@@ -8,7 +8,7 @@ try{
 
   $query_string = "select * from games where name_game = '"+ $game+ "';" ;
 
-  $games = $db->query($y);
+  
   foreach ($db->query($query_string) as $row) {
   echo("<li class='game'>The game <b>"+$row['game_name']+"</b> It has the genre of"+ $row['genre'] +" and is in stock</li></br></br>");
 
